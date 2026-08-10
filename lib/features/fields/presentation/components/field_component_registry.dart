5ebd4995365dcbd9068eb1e9b4bec9556b6f704e
KeksPeks <60257263+KeksPeks@users.noreply.github.com>
2026-08-10 23:37:31 +0300

    Подключить компонент дробного числа в реестр полей

--- lib/features/fields/presentation/components/field_component_registry.dart
+++ lib/features/fields/presentation/components/field_component_registry.dart
@@ -1,26 +1,23 @@
 import '../../domain/types/field_type.dart';
 
+import 'decimal_field_component.dart';
 import 'field_component.dart';
 import 'integer_field_component.dart';
 import 'text_field_component.dart';
 
+/// Реестр компонентов ввода для типов полей.
 class FieldComponentRegistry {
+  const FieldComponentRegistry();
 
   FieldComponent component(FieldType type) {
-
     switch (type) {
-
       case FieldType.integer:
         return const IntegerFieldComponent();
-
+      case FieldType.decimal:
+        return const DecimalFieldComponent();
       case FieldType.text:
-        return const TextFieldComponent();
-
       default:
         return const TextFieldComponent();
-
     }
-
   }
-
 }
