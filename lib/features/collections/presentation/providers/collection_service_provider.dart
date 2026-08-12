8c28b6e330ed52d7825c6733e63655e9d84b3eb7
KeksPeks <60257263+KeksPeks@users.noreply.github.com>
2026-08-12 20:17:02 +0300

    Подключить сервис полей к созданию коллекций

--- lib/features/collections/presentation/providers/collection_service_provider.dart
+++ lib/features/collections/presentation/providers/collection_service_provider.dart
@@ -2,32 +2,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../domain/factories/collection_factory.dart';
 import '../../domain/services/collection_service.dart';
-
+import '../../../fields/presentation/providers/field_service_provider.dart';
 import 'collection_repository_provider.dart';
 
-
-
-final collectionServiceProvider =
-    Provider<CollectionService>((ref) {
-
-
-
-  final repository =
-      ref.watch(
-        collectionRepositoryProvider,
-      );
-
-
+/// Провайдер сервиса коллекций.
+final collectionServiceProvider = Provider<CollectionService>((ref) {
+  final repository = ref.watch(collectionRepositoryProvider);
+  final fieldService = ref.watch(fieldServiceProvider).requireValue;
 
   return CollectionService(
-
-    repository:
-        repository,
-
-    factory:
-        CollectionFactory(),
-
+    repository: repository,
+    factory: CollectionFactory(),
+    fieldService: fieldService,
   );
-
-
 });
