1fd2796e35924d81af81fe024bc167984fa93732
KeksPeks <60257263+KeksPeks@users.noreply.github.com>
2026-08-16 18:37:12 +0300

    feat: use adaptive app shell as application entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
-import 'app/app.dart';
-
-
+import 'app/adaptive_app.dart';
 
 void main() {
-
-
   runApp(
-
     const ProviderScope(
-
-      child: MyApp(),
-
+      child: ResponsiveCollectionApp(),
     ),
-
   );
-
-
 }
