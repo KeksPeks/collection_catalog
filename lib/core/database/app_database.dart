d451b49c0cd5b5da5ac4a2c23c237fb8a297d453
KeksPeks <60257263+KeksPeks@users.noreply.github.com>
2026-08-17 00:09:41 +0300

    fix: migrate collection section sort order

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -25,7 +25,7 @@ class AppDatabase extends _$AppDatabase {
   );
 
   @override
-  int get schemaVersion => 4;
+  int get schemaVersion => 5;
 
   @override
   MigrationStrategy get migration => MigrationStrategy(
@@ -56,6 +56,13 @@ class AppDatabase extends _$AppDatabase {
 
             await m.createTable(
               itemAttachmentTable,
+            );
+          }
+
+          if (from < 5) {
+            await m.addColumn(
+              collectionSectionTable,
+              collectionSectionTable.sortOrder,
             );
           }
         },
