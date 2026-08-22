d489487bb86fa415a8c6156897b57085b8e58935
KeksPeks <60257263+KeksPeks@users.noreply.github.com>
2026-08-22 11:08:29 +0300

    feat: add catalog version metadata

--- lib/features/catalogs/domain/entities/catalog_definition.dart
+++ lib/features/catalogs/domain/entities/catalog_definition.dart
@@ -1,7 +1,6 @@
 import '../../../templates/domain/entities/template.dart';
 import 'catalog_entry_definition.dart';
 
-/// Универсальное описание готового каталога.
 class CatalogDefinition {
   final String id;
   final String name;
@@ -10,34 +9,17 @@ class CatalogDefinition {
   final Template template;
   final int? totalItems;
   final List<CatalogSectionDefinition> sections;
-
-  /// Поле, по которому записи открываются по умолчанию.
   final String primaryField;
-
-  /// Демонстрационные записи. Сервер сможет заменить их реальными данными.
   final List<CatalogEntryDefinition> entries;
-
-  const CatalogDefinition({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.templateId,
-    required this.template,
-    this.totalItems,
-    this.sections = const [],
-    this.primaryField = '',
-    this.entries = const [],
-  });
+  final int version;
+  final DateTime? publishedAt;
+  final List<String> changes;
+  const CatalogDefinition({required this.id, required this.name, required this.description, required this.templateId, required this.template, this.totalItems, this.sections = const [], this.primaryField = '', this.entries = const [], this.version = 1, this.publishedAt, this.changes = const []});
 }
 
 class CatalogSectionDefinition {
   final String id;
   final String name;
   final List<CatalogSectionDefinition> children;
-
-  const CatalogSectionDefinition({
-    required this.id,
-    required this.name,
-    this.children = const [],
-  });
+  const CatalogSectionDefinition({required this.id, required this.name, this.children = const []});
 }
