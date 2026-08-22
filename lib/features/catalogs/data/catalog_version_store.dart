0b53420c5baf76d14bd4570d7f6bebf3efb21621
KeksPeks <60257263+KeksPeks@users.noreply.github.com>
2026-08-22 10:58:01 +0300

    feat: track installed catalog versions

---
+++ lib/features/catalogs/data/catalog_version_store.dart
@@ -0,0 +1,23 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CatalogVersionStore {
+  CatalogVersionStore._();
+
+  static String _key(String catalogId) => 'catalog.installedVersion.$catalogId';
+
+  static Future<int?> installedVersion(String catalogId) async {
+    final preferences = await SharedPreferences.getInstance();
+    await preferences.reload();
+    return preferences.getInt(_key(catalogId));
+  }
+
+  static Future<void> markInstalled(String catalogId, int version) async {
+    final preferences = await SharedPreferences.getInstance();
+    await preferences.setInt(_key(catalogId), version);
+  }
+
+  static Future<bool> needsUpdate(String catalogId, int currentVersion) async {
+    final installed = await installedVersion(catalogId);
+    return installed != null && installed < currentVersion;
+  }
+}
