555a25d87b823ec5b65ecc5052863107f2d50aa3
KeksPeks <60257263+KeksPeks@users.noreply.github.com>
2026-08-22 19:57:21 +0300

    feat: use integrated collector tools app shell

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'app/adaptive_app.dart';
+import 'app/collection_catalog_app.dart';
 
 void main() {
   runApp(
     const ProviderScope(
-      child: ResponsiveCollectionApp(),
+      child: CollectionCatalogApp(),
     ),
   );
 }
