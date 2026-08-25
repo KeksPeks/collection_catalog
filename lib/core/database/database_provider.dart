0ab5c6c1b5db1b7c86225d0288cc76ade9169cde
KeksPeks <60257263+KeksPeks@users.noreply.github.com>
2026-08-25 21:26:40 +0300

    fix: restore extended SQLite tables on startup

--- lib/core/database/database_provider.dart
+++ lib/core/database/database_provider.dart
@@ -3,32 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app_database.dart';
 import 'database_connection.dart';
 
-
-
-final databaseProvider =
-
-    FutureProvider<AppDatabase>(
-
-
-  (ref) async {
-
-
-    ref.keepAlive();
-
-
-
-    final connection =
-
-        await openDatabaseConnection();
-
-
-
-    return AppDatabase(
-      connection,
-    );
-
-
-  },
-
-
-);
+final databaseProvider = FutureProvider<AppDatabase>((ref) async {
+  ref.keepAlive();
+  final connection = await openDatabaseConnection();
+  final database = AppDatabase(connection);
+  await database.ensureCatalogItemTables();
+  await database.ensureStorageTables();
+  return database;
+});
